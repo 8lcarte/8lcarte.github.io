686f83844240c0b82bcb70e3546091edcbd48b71
8lcarte <8lcarte@gmail.com>
2015-02-15 16:45:15 -0500

    updated shopping list and added to portfolio page

--- shopping-list/js/main.js
+++ shopping-list/js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
 //ADDING ITEMS WHEN BUTTON IS PRESSED
 	$('#submit').click(function(){
-		console.log("submit clicked")
 		var txtbox = document.getElementById('input');
 		var txtval = txtbox.value;
 		event.preventDefault();
@@ -13,6 +12,14 @@ $(document).ready(function() {
 		document.getElementById('input').value = '';
 		};
 	});
+//DELETING ITEMS OFF THE LIST
+	$('#list').on('click', '.delete', function(e){
+		e.preventDefault(); 
+		$(this).parent().remove()
+	});
+	//SORTABLE LIST ITEMS?  HOW DOES THIS WORK?
+	//$('#list').sortable({ axis: "y" });
+
 
 //CROSSING OFF ITEMS WHEN CLICKED
   $('#list').on('click', 'li', function(){
@@ -22,6 +29,14 @@ $(document).ready(function() {
   // fires when any LIs are clicked on
   // including LIs that aren't on the page when it is initially loaded
 	})
+//USING ENTER TO ENTER AN ITEM
+$('#input').keyup(function(event){
+		if(event.keyCode == 13) {
+			event.preventDefault();
+			$('#submit').click();
+		};
+	});	
+
 
 
 });
