7a410566ca5fa2f273ddbafe7bac95895b3ec1b4
8lcarte <8lcarte@gmail.com>
2015-02-18 20:10:27 -0500

    made shopping list a little nicer and more responsive

--- shopping-list/js/main.js
+++ shopping-list/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
 //ADDING ITEMS WHEN BUTTON IS PRESSED
-	$('#submit').click(function(){
+	$('#submit').click(function(e){
 		var txtbox = document.getElementById('input');
 		var txtval = txtbox.value;
-		event.preventDefault();
+		e.preventDefault();
 
 		if(!$.trim($('#item').val())) {
 			$('<li class="items"></li>').appendTo('#list').html('<div class="check"></div><span class="item">' + txtval +'</span><img class="delete" src="images/delete.png">');
@@ -17,22 +17,26 @@ $(document).ready(function() {
 		e.preventDefault(); 
 		$(this).parent().remove()
 	});
-	//SORTABLE LIST ITEMS?  HOW DOES THIS WORK?
-	//$('#list').sortable({ axis: "y" });
+//SORTABLE LIST 
+	$('#list').sortable({ 
+		options: {
+		axis: "y", 
+		distance: 500 //why isn't my second option working
+	}
+	});
 
 
 //CROSSING OFF ITEMS WHEN CLICKED
   $('#list').on('click', 'li', function(){
-  	console.log("clicked")
   	$(this).toggleClass('strike');
   	$(this).children('.check').toggleClass('Checked');
   // fires when any LIs are clicked on
   // including LIs that aren't on the page when it is initially loaded
 	})
 //USING ENTER TO ENTER AN ITEM
-$('#input').keyup(function(event){
-		if(event.keyCode == 13) {
-			event.preventDefault();
+$('#input').keyup(function(e){
+		if(e.keyCode == 13) {
+			e.preventDefault();
 			$('#submit').click();
 		};
 	});	
