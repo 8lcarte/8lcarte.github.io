2f047f4130ac79a2da01d48b3e559443468b3a59
8lcarte <8lcarte@gmail.com>
2015-02-18 22:37:35 -0500

    finished javascript conditionals

---
+++ Javascript Exercises/rain-snow.js
@@ -0,0 +1,31 @@
+// var rain = false
+// var snow = true
+// var boots = false
+// var umbrella = false
+// if (rain) {
+// 	print("It's raining grab an umbrella");
+// 	var umbrella = true;
+// }
+// else if (snow) {
+// 	print("It's snowing grab some boots");
+// 	var boots = true;
+// }
+// else {
+// 	print("It's sunny, go to the beach");
+// }
+
+var myName = "Lee Carter";
+if (myName > 10) {
+	print("More than 10 letters");
+}
+else {
+	print("Less than 10 letters");
+}
+
+// falsy values:
+// false
+// 0
+// ""
+// null
+// NaN
+// undefined
