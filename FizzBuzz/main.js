98c0b2840aff464bbd12e9d16c3bc552daf13196
8lcarte <8lcarte@gmail.com>
2015-02-20 16:24:55 -0500

    finished FizzBuzz refactor

--- FizzBuzz/main.js
+++ FizzBuzz/main.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
+
+//fizzBuzz function
+function fizzBuzz(userVal) {
+	for (var i = 1; i < userVal; i++) {
+		if (i % 5 == 0 && i % 3 ==0) {
+			document.write("fizzbuzz<br>");
+		}
+		else if (i % 3 == 0){
+			document.write("fizz<br>");
+		}
+		else if (i % 5 == 0 ){
+			document.write("buzz<br>");
+		}
+		else {
+			document.write(i + "<br>");					
+		}
+	}
+}
+
+
+//Click for start
 	$('#submit').click(function(){
-			for (var i = 1; i < 100; i++) {
-				if (i % 5 == 0 && i % 3 ==0) {
-					document.write("fizzbuzz<br>")
-				}
-				else if (i % 3 == 0){
-					document.write("fizz<br>")
-				}
-				else if (i % 5 == 0 ){
-					document.write("buzz<br>")
-				}
-				else {
-					document.write(i + "<br>")					
-				}
-			}
+		var txtbox = document.getElementById('input');
+		var txtval = txtbox.value;
+		fizzBuzz(txtval);	
 	});	
+
 });
